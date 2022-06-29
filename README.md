e59fbeab5fae8559232c0230521a92e65fdbaa02
Christian Burtt <christian.b@forefrontethiopia.com>
2022-06-29 20:21:58 +0300

    add sql script and update readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # shareposts
+
+## About
+SharePosts is a **very** simple social media platform built on the "basicMVC" framework, a slight variation of the TraversyMVC by Brad Traversy through a udemy class.
+
+It is nothing special, just a chance to build an MVC framework and a simple app on top of it. I don't think I'll be using this in place of CodeIgniter or Laravel anytimesoon, but the practice to build it out is helpful
+
+## Installation
+### SQL DB
+Run the SQL script to create a DB. Ensure you create a user and pw and grant permissions
+
+### PHP Framework
+Simply copy the files to the public html folder on your web server
+ - Update DB and URLROOT info in app/config/config.php
+ - Update the .htaccess file in public/.htaccess to point to the same folder as URLROOT
+ 
+ That's it!
+
+ 
---
+++ shareposts.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jun 29, 2022 at 05:21 PM
+-- Server version: 5.7.32
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shareposts`
+--
+CREATE DATABASE IF NOT EXISTS `shareposts` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `shareposts`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pw` varchar(255) NOT NULL,
+  `user_created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
